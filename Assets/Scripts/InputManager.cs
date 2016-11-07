4c748b7f54d4683d57f7408b3558d5b8e767bd21
Irelevance <nimesh.shrestha@hotmail.com>
2016-11-06 22:58:14 -0800

    Added joypad input

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class InputManager {
+
+	public static bool isUpPressed(int joypadNumber) {
+
+        return Input.GetAxis("Vertical" + joypadNumber) > 0;
+    }
+
+    public static bool isDownPressed(int joypadNumber) {
+
+        return Input.GetAxis("Vertical" + joypadNumber) < 0;
+    }
+
+    public static bool isLeftPressed(int joypadNumber) {
+        
+        return Input.GetAxis("Horizontal" + joypadNumber) < 0;
+    }
+
+    public static bool isRightPressed(int joypadNumber) {
+
+        return Input.GetAxis("Horizontal" + joypadNumber) > 0;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rigidBody;
+
+    public float speed;
+    public float rotationalSpeed;
+
+    public int joypadNumber = 3;
+
+	// Use this for initialization
+	void Start () {
+	    
+        rigidBody = GetComponent<Rigidbody2D>() as Rigidbody2D;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        Vector2 targetVelocity = Vector2.zero;
+
+        if(InputManager.isUpPressed(joypadNumber))
+            targetVelocity = transform.up * speed;
+        
+        if(InputManager.isDownPressed(joypadNumber))
+            targetVelocity = -transform.up * speed;
+
+        float rotationalVelocity = 0;
+        
+        //rotate to left or right
+        if(InputManager.isLeftPressed(joypadNumber))
+            rotationalVelocity = rotationalSpeed * (InputManager.isDownPressed(joypadNumber) ? -1 : 1);
+
+        if (InputManager.isRightPressed(joypadNumber))
+            rotationalVelocity = -rotationalSpeed * (InputManager.isDownPressed(joypadNumber) ? -1 : 1);
+
+        Vector2 velocityChange = targetVelocity - rigidBody.velocity;
+
+        rigidBody.AddForce(velocityChange, ForceMode2D.Impulse);
+        rigidBody.angularVelocity = rotationalVelocity;
+	}
+
+    
+}
